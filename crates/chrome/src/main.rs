0fd689251b00caca665b4152189cdab71040f72c
Takagi Tasuku <tasuren@outlook.jp>
2025-07-31 12:07:34 +0900

    右クリックでクラッシュしないよう実装

--- crates/chrome/src/main.rs
+++ crates/chrome/src/main.rs
@@ -309,6 +309,11 @@ fn main() {
         1
     );
 
+    #[cfg(target_os = "macos")]
+    unsafe {
+        macos::extend_nswindow_class()
+    };
+
     run_message_loop();
 
     let window = window.lock().expect("Failed to lock window");
@@ -316,4 +321,49 @@ fn main() {
     assert!(window.has_one_ref());
 
     shutdown();
+}
+
+#[cfg(target_os = "macos")]
+mod macos {
+    use objc2::{runtime::*, *};
+
+    extern "C" fn set_handling_send_event(
+        _this: *mut AnyObject,
+        _cmd: Sel,
+        _handling_send_event: Bool,
+    ) {
+    }
+
+    extern "C" fn is_handling_send_event(_this: *mut AnyObject, _cmd: Sel) -> Bool {
+        Bool::YES
+    }
+
+    pub unsafe fn extend_nswindow_class() {
+        let ns_window = class!(NSApplication);
+
+        let encoding_get = c"B@:";
+        let encoding_set = c"v@:B";
+
+        let _ = unsafe {
+            objc2::ffi::class_addMethod(
+                ns_window as *const _ as *mut _,
+                sel!(isHandlingSendEvent),
+                std::mem::transmute::<*const (), unsafe extern "C-unwind" fn()>(
+                    is_handling_send_event as *const (),
+                ),
+                encoding_get.as_ptr(),
+            )
+        };
+
+        let _ = unsafe {
+            objc2::ffi::class_addMethod(
+                ns_window as *const _ as *mut _,
+                sel!(setHandlingSendEvent:),
+                std::mem::transmute::<*const (), unsafe extern "C-unwind" fn()>(
+                    set_handling_send_event as *const (),
+                ),
+                encoding_set.as_ptr(),
+            )
+        };
+    }
 }
