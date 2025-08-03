b32d63937c9e37a0393035a870fa2d103a0efbfc
Takagi Tasuku <tasuren@outlook.jp>
2025-08-03 11:04:30 +0900

    OSRとOSRじゃないやつの二つを格納

---
+++ crates/cef-rs-sample/src/main.rs
@@ -0,0 +1,383 @@
+use cef::{args::Args, rc::*, *};
+use std::sync::{Arc, Mutex};
+
+struct DemoApp {
+    object: *mut RcImpl<cef::sys::_cef_app_t, Self>,
+    window: Arc<Mutex<Option<Window>>>,
+}
+
+impl DemoApp {
+    fn new_app(window: Arc<Mutex<Option<Window>>>) -> App {
+        App::new(Self {
+            object: std::ptr::null_mut(),
+            window,
+        })
+    }
+}
+
+impl WrapApp for DemoApp {
+    fn wrap_rc(&mut self, object: *mut RcImpl<cef::sys::_cef_app_t, Self>) {
+        self.object = object;
+    }
+}
+
+impl Clone for DemoApp {
+    fn clone(&self) -> Self {
+        let object = unsafe {
+            let rc_impl = &mut *self.object;
+            rc_impl.interface.add_ref();
+            self.object
+        };
+        let window = self.window.clone();
+
+        Self { object, window }
+    }
+}
+
+impl Rc for DemoApp {
+    fn as_base(&self) -> &cef::sys::cef_base_ref_counted_t {
+        unsafe {
+            let base = &*self.object;
+            std::mem::transmute(&base.cef_object)
+        }
+    }
+}
+
+impl ImplApp for DemoApp {
+    fn get_raw(&self) -> *mut cef::sys::_cef_app_t {
+        self.object.cast()
+    }
+
+    fn browser_process_handler(&self) -> Option<BrowserProcessHandler> {
+        Some(DemoBrowserProcessHandler::new_browser_process_handler(
+            self.window.clone(),
+        ))
+    }
+
+    fn on_before_command_line_processing(
+        &self,
+        _process_type: Option<&CefString>,
+        command_line: Option<&mut CommandLine>,
+    ) {
+        if let Some(command_line) = command_line {
+            // キーチェーンのパスワード入力をデバッグ時は防止。
+            #[cfg(debug_assertions)]
+            command_line.append_switch(Some(&"use-mock-keychain".into()));
+        }
+    }
+}
+
+struct DemoBrowserProcessHandler {
+    object: *mut RcImpl<cef::sys::cef_browser_process_handler_t, Self>,
+    window: Arc<Mutex<Option<Window>>>,
+}
+
+impl DemoBrowserProcessHandler {
+    fn new_browser_process_handler(window: Arc<Mutex<Option<Window>>>) -> BrowserProcessHandler {
+        BrowserProcessHandler::new(Self {
+            object: std::ptr::null_mut(),
+            window,
+        })
+    }
+}
+
+impl Rc for DemoBrowserProcessHandler {
+    fn as_base(&self) -> &cef::sys::cef_base_ref_counted_t {
+        unsafe {
+            let base = &*self.object;
+            std::mem::transmute(&base.cef_object)
+        }
+    }
+}
+
+impl WrapBrowserProcessHandler for DemoBrowserProcessHandler {
+    fn wrap_rc(&mut self, object: *mut RcImpl<cef::sys::_cef_browser_process_handler_t, Self>) {
+        self.object = object;
+    }
+}
+
+impl Clone for DemoBrowserProcessHandler {
+    fn clone(&self) -> Self {
+        let object = unsafe {
+            let rc_impl = &mut *self.object;
+            rc_impl.interface.add_ref();
+            rc_impl
+        };
+
+        let window = self.window.clone();
+
+        Self { object, window }
+    }
+}
+
+impl ImplBrowserProcessHandler for DemoBrowserProcessHandler {
+    fn get_raw(&self) -> *mut cef::sys::_cef_browser_process_handler_t {
+        self.object.cast()
+    }
+
+    // The real lifespan of cef starts from `on_context_initialized`, so all the cef objects should be manipulated after that.
+    fn on_context_initialized(&self) {
+        println!("cef context intiialized");
+
+        let mut client = DemoClient::new_client();
+        let url = CefString::from("chrome://settings/");
+
+        let browser_view = browser_view_create(
+            Some(&mut client),
+            Some(&url),
+            Some(&Default::default()),
+            Option::<&mut DictionaryValue>::None,
+            Option::<&mut RequestContext>::None,
+            Option::<&mut BrowserViewDelegate>::None,
+        )
+        .expect("Failed to create browser view");
+
+        let mut delegate = DemoWindowDelegate::new_window_delegate(browser_view);
+        if let Ok(mut window) = self.window.lock() {
+            *window = Some(
+                window_create_top_level(Some(&mut delegate)).expect("Failed to create window"),
+            );
+        }
+    }
+}
+
+struct DemoClient(*mut RcImpl<cef::sys::_cef_client_t, Self>);
+
+impl DemoClient {
+    fn new_client() -> Client {
+        Client::new(Self(std::ptr::null_mut()))
+    }
+}
+
+impl WrapClient for DemoClient {
+    fn wrap_rc(&mut self, object: *mut RcImpl<cef::sys::_cef_client_t, Self>) {
+        self.0 = object;
+    }
+}
+
+impl Clone for DemoClient {
+    fn clone(&self) -> Self {
+        unsafe {
+            let rc_impl = &mut *self.0;
+            rc_impl.interface.add_ref();
+        }
+
+        Self(self.0)
+    }
+}
+
+impl Rc for DemoClient {
+    fn as_base(&self) -> &cef::sys::cef_base_ref_counted_t {
+        unsafe {
+            let base = &*self.0;
+            std::mem::transmute(&base.cef_object)
+        }
+    }
+}
+
+impl ImplClient for DemoClient {
+    fn get_raw(&self) -> *mut cef::sys::_cef_client_t {
+        self.0.cast()
+    }
+}
+
+struct DemoWindowDelegate {
+    base: *mut RcImpl<cef::sys::_cef_window_delegate_t, Self>,
+    browser_view: BrowserView,
+}
+
+impl DemoWindowDelegate {
+    fn new_window_delegate(browser_view: BrowserView) -> WindowDelegate {
+        WindowDelegate::new(Self {
+            base: std::ptr::null_mut(),
+            browser_view,
+        })
+    }
+}
+
+impl WrapWindowDelegate for DemoWindowDelegate {
+    fn wrap_rc(&mut self, object: *mut RcImpl<cef::sys::_cef_window_delegate_t, Self>) {
+        self.base = object;
+    }
+}
+
+impl Clone for DemoWindowDelegate {
+    fn clone(&self) -> Self {
+        unsafe {
+            let rc_impl = &mut *self.base;
+            rc_impl.interface.add_ref();
+        }
+
+        Self {
+            base: self.base,
+            browser_view: self.browser_view.clone(),
+        }
+    }
+}
+
+impl Rc for DemoWindowDelegate {
+    fn as_base(&self) -> &cef::sys::cef_base_ref_counted_t {
+        unsafe {
+            let base = &*self.base;
+            std::mem::transmute(&base.cef_object)
+        }
+    }
+}
+
+impl ImplViewDelegate for DemoWindowDelegate {
+    fn on_child_view_changed(
+        &self,
+        _view: Option<&mut View>,
+        _added: ::std::os::raw::c_int,
+        _child: Option<&mut View>,
+    ) {
+        // view.as_panel().map(|x| x.as_window().map(|w| w.close()));
+    }
+
+    fn get_raw(&self) -> *mut cef::sys::_cef_view_delegate_t {
+        self.base.cast()
+    }
+}
+
+impl ImplPanelDelegate for DemoWindowDelegate {}
+
+impl ImplWindowDelegate for DemoWindowDelegate {
+    fn on_window_created(&self, window: Option<&mut Window>) {
+        if let Some(window) = window {
+            let view = self.browser_view.clone();
+            window.add_child_view(Some(&mut (&view).into()));
+            window.show();
+        }
+    }
+
+    fn on_window_destroyed(&self, _window: Option<&mut Window>) {
+        quit_message_loop();
+    }
+
+    fn with_standard_window_buttons(&self, _window: Option<&mut Window>) -> ::std::os::raw::c_int {
+        1
+    }
+
+    fn can_resize(&self, _window: Option<&mut Window>) -> ::std::os::raw::c_int {
+        1
+    }
+
+    fn can_maximize(&self, _window: Option<&mut Window>) -> ::std::os::raw::c_int {
+        1
+    }
+
+    fn can_minimize(&self, _window: Option<&mut Window>) -> ::std::os::raw::c_int {
+        1
+    }
+
+    fn can_close(&self, _window: Option<&mut Window>) -> ::std::os::raw::c_int {
+        1
+    }
+}
+
+// FIXME: Rewrite this demo based on cef/tests/cefsimple
+fn main() {
+    #[cfg(target_os = "macos")]
+    let _loader = {
+        let loader = library_loader::LibraryLoader::new(&std::env::current_exe().unwrap(), false);
+        assert!(loader.load());
+        loader
+    };
+
+    let _ = api_hash(sys::CEF_API_VERSION_LAST, 0);
+
+    let args = Args::new();
+    let cmd = args.as_cmd_line().unwrap();
+
+    let switch = CefString::from("type");
+    let is_browser_process = cmd.has_switch(Some(&switch)) != 1;
+
+    let window = Arc::new(Mutex::new(None));
+    let mut app = DemoApp::new_app(window.clone());
+
+    let ret = execute_process(
+        Some(args.as_main_args()),
+        Some(&mut app),
+        std::ptr::null_mut(),
+    );
+
+    if is_browser_process {
+        println!("launch browser process");
+        assert!(ret == -1, "cannot execute browser process");
+    } else {
+        let process_type = CefString::from(&cmd.switch_value(Some(&switch)));
+        println!("launch process {process_type}");
+        assert!(ret >= 0, "cannot execute non-browser process");
+        // non-browser process does not initialize cef
+        return;
+    }
+
+    let settings = Settings::default();
+    assert_eq!(
+        initialize(
+            Some(args.as_main_args()),
+            Some(&settings),
+            Some(&mut app),
+            std::ptr::null_mut(),
+        ),
+        1
+    );
+
+    #[cfg(target_os = "macos")]
+    unsafe {
+        macos::extend_nswindow_class()
+    };
+
+    run_message_loop();
+
+    let window = window.lock().expect("Failed to lock window");
+    let window = window.as_ref().expect("Window is None");
+    assert!(window.has_one_ref());
+
+    shutdown();
+}
+
+#[cfg(target_os = "macos")]
+mod macos {
+    use objc2::{runtime::*, *};
+
+    extern "C" fn set_handling_send_event(
+        _this: *mut AnyObject,
+        _cmd: Sel,
+        _handling_send_event: Bool,
+    ) {
+    }
+
+    extern "C" fn is_handling_send_event(_this: *mut AnyObject, _cmd: Sel) -> Bool {
+        Bool::YES
+    }
+
+    pub unsafe fn extend_nswindow_class() {
+        let ns_window = class!(NSApplication);
+
+        let encoding_get = c"B@:";
+        let encoding_set = c"v@:B";
+
+        let _ = unsafe {
+            objc2::ffi::class_addMethod(
+                ns_window as *const _ as *mut _,
+                sel!(isHandlingSendEvent),
+                std::mem::transmute::<*const (), unsafe extern "C-unwind" fn()>(
+                    is_handling_send_event as *const (),
+                ),
+                encoding_get.as_ptr(),
+            )
+        };
+
+        let _ = unsafe {
+            objc2::ffi::class_addMethod(
+                ns_window as *const _ as *mut _,
+                sel!(setHandlingSendEvent:),
+                std::mem::transmute::<*const (), unsafe extern "C-unwind" fn()>(
+                    set_handling_send_event as *const (),
+                ),
+                encoding_set.as_ptr(),
+            )
+        };
+    }
+}
