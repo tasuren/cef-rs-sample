ddf1d9ffa2a2b487b0b8c3588e6bee9551566fa0
Takagi Tasuku <tasuren@outlook.jp>
2025-08-04 21:03:12 +0900

    Remove unused debug logs

--- crates/cef-rs-osr-sample/src/main.rs
+++ crates/cef-rs-osr-sample/src/main.rs
@@ -18,7 +18,6 @@ fn main() -> std::process::ExitCode {
         loader
     };
 
-    println!("22");
     let event_loop = EventLoop::with_user_event().build().unwrap();
 
     let _ = api_hash(sys::CEF_API_VERSION_LAST, 0);
@@ -29,7 +28,6 @@ fn main() -> std::process::ExitCode {
     let switch = CefString::from("type");
     let is_browser_process = cmd.has_switch(Some(&switch)) != 1;
 
-    println!("11");
     let mut app = cef_impl::SampleApp::new_app(PumpCefHandle::new(event_loop.create_proxy()));
 
     let ret = execute_process(
